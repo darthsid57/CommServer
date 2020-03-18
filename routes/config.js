5a7a73fdb38823896f3eb2d4e10be51829aa5551
darthsid57 <siddhantanand23@gmail.com>
2020-03-18 20:00:42 +1200

    First

---
+++ routes/config.js
@@ -0,0 +1,6 @@
+var user = "sa";
+var password = "Welcome1";
+var database = "Communications";
+var server = "PROGRAMMERICT";
+
+module.exports = { user, password, server, database };
---
+++ routes/server.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var router = express.Router();
+
+var bodyparser = require("body-parser");
+var mssql = require("mssql");
+var cors = require("cors");
+
+router.use(cors());
+
+router.use(bodyparser.json());
+router.use(bodyparser.urlencoded({ extended: false }));
+
+var config2 = require("./config");
+
+const config = {
+  user: config2.user,
+  password: config2.password,
+  server: config2.server,
+  database: config2.database,
+
+  pool: {
+    max: 100,
+    min: 0,
+    idleTimeoutMillis: 30000
+  }
+};
+
+router.get("/district", async function(req, resp) {
+  try {
+    await new mssql.ConnectionPool(config)
+      .connect()
+      .then(pool => {
+        return pool.request().query(`SELECT * FROM TM_District;`);
+      })
+      .then(result => {
+        let rows = result.recordset;
+        resp.status(200).json(rows);
+      });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get("/region", async function(req, resp) {
+  try {
+    await new mssql.ConnectionPool(config)
+      .connect()
+      .then(pool => {
+        return pool.request().query(`SELECT * FROM TM_Region;`);
+      })
+      .then(result => {
+        let rows = result.recordset;
+        resp.status(200).json(rows);
+      });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get("/commendation", async function(req, resp) {
+  try {
+    await new mssql.ConnectionPool(config)
+      .connect()
+      .then(pool => {
+        return pool.request().query(`SELECT * FROM T_Communication;`);
+      })
+      .then(result => {
+        let rows = result.recordset;
+        resp.status(200).json(rows);
+      });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("server active");
+});
+
+module.exports = router;
