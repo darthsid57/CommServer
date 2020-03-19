eb283a0736dcdcdddb14478c776eee0e0ae70a7b
darthsid57 <siddhantanand23@gmail.com>
2020-03-19 16:29:45 +1200

    config.js server name change

--- routes/config.js
+++ routes/config.js
@@ -1,6 +1,7 @@
 var user = "sa";
 var password = "Welcome1";
 var database = "Communications";
-var server = "DESKTOP-6DOGOA2\\SIDSERVER";
+// var server = "DESKTOP-6DOGOA2\\SIDSERVER";
+var server = "PROGRAMMERICT";
 
 module.exports = { user, password, server, database };
