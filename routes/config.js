a7d48565d1188ba24ad1efe58d1b509832c7e3a4
darthsid57 <siddhantanand23@gmail.com>
2020-04-01 17:49:49 +1200

    v2

--- routes/config.js
+++ routes/config.js
@@ -2,6 +2,7 @@ var user = "sa";
 var password = "Welcome1";
 var database = "Communications";
 // var server = "DESKTOP-6DOGOA2\\SIDSERVER";
-var server = "PROGRAMMERICT";
+// var server = "PROGRAMMERICT";
+var server = "STARKILLERBASE";
 
 module.exports = { user, password, server, database };
